ed1e42f5ea30440a22b034b1be5b2949133f0052
Patrick Johnstone <patrick.johnstone@spektrix.com>
2022-12-02 10:21:37 +0000

    Day2: calculate final score

--- fsharp/day2/day2.fsx
+++ fsharp/day2/day2.fsx
@@ -8,12 +8,31 @@ type Shape = {
   Beats: string
 }
 
-let (opMove,yourMove) =
+let (opMove, yourMove) =
   lines
   |> List.map (fun (line: string) -> line.Remove(1,1))
   |> List.map (fun line -> Seq.toList line |> List.splitAt 1)
   |> List.map (fun (f,s) -> (List.exactlyOne f, List.exactlyOne s))
   |> List.unzip
 
-printfn "Opponent's moves: %A" opMove
-printfn "Your moves: %A" yourMove
+let makeShape char =
+  match char with
+  | 'A' | 'X' -> { Name = "Rock"; Value = 1; Beats = "Scissors" }
+  | 'B' | 'Y' -> { Name = "Paper"; Value = 2; Beats = "Rock" }
+  | _ -> { Name = "Scissors"; Value = 3; Beats = "Paper" }
+
+let (opShapes, yourShapes) =
+  (opMove, yourMove)
+  |> fun (f,s) -> (f |> List.map makeShape, s |> List.map makeShape)
+
+let scoreRound opShape yourShape =
+  match (opShape.Name = yourShape.Name) with
+  | true -> 3 + yourShape.Value
+  | false ->
+    match (opShape.Name = yourShape.Beats) with
+    | true -> 6 + yourShape.Value
+    | false -> 0 + yourShape.Value
+
+let finalScore = List.fold2 (fun acc o y -> acc + scoreRound o y) 0 opShapes yourShapes
+
+printfn "Your final score is: %i" finalScore
