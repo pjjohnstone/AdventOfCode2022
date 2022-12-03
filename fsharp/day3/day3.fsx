dcb0695d805e4b0bd3238bc075367ad4163dc324
Patrick Johnstone <johnstonep@gmail.com>
2022-12-03 14:36:22 +0000

    Day3: part2 solve for badge priority

--- fsharp/day3/day3.fsx
+++ fsharp/day3/day3.fsx
@@ -11,14 +11,19 @@ let getBackpacks lines =
   lines
   |> List.map dividePartitions
 
-let existsInList ch array =
+let existsInArray ch array =
   if Array.contains ch array then Some(ch) else None
+
+let existsInArrays ch array1 array2 =
+  if Array.contains ch array1 && Array.contains ch array2
+  then Some(ch)
+  else None
 
 let getDuplicateItems (partitionOne: char[]) (partitionTwo: char[]) =
   let partTwoDupes =
-    partitionOne |> Array.choose (fun item -> existsInList item partitionTwo)
+    partitionOne |> Array.choose (fun item -> existsInArray item partitionTwo)
   let partOneDupes =
-    partitionTwo |> Array.choose (fun item -> existsInList item partitionOne)
+    partitionTwo |> Array.choose (fun item -> existsInArray item partitionOne)
   Array.concat [partOneDupes; partTwoDupes]
 
 let getDuplicates (backpacks: char[] list list) =
@@ -30,15 +35,33 @@ let scoreItem (char: char) =
   | false -> (char |> int32) - 96
   | true -> (char |> int32) - 38
 
-let scoreDuplicates (dupes: char[]) =
-  dupes
-  |> Array.map (fun item -> scoreItem item)
-  |> Array.sum
+let getGroups (lines: string list) =
+  let rec getGroupsRec lines (groups: string list list) =
+    match lines with
+    | [] -> groups
+    | _ ->
+      getGroupsRec (List.removeManyAt 0 3 lines) ((List.take 3 lines)::groups)
+  getGroupsRec lines []
+
+let findBadgeInBackpacks (backpacks: char[] list) =
+  backpacks[0]
+  |> Array.pick (fun item -> existsInArrays item backpacks[1] backpacks[2])
+
+let getBadge (group: string list) =
+  group
+  |> List.map (fun str -> str |> Seq.toArray)
+  |> findBadgeInBackpacks
 
 let backpacks = getBackpacks lines
 let duplicates = getDuplicates backpacks
 let score =
   duplicates
   |> List.map (fun a -> Array.head a)
+  |> List.map scoreItem
+  |> List.sum
+let groups = getGroups lines
+let badgePriorities =
+  groups
+  |> List.map getBadge
   |> List.map scoreItem
   |> List.sum
