e56436013ab631a741c4c4af817ebd40eb0834d8
Patrick Johnstone <johnstonep@gmail.com>
2022-12-05 17:58:33 +0000

    Day5: part1: that was rough

--- fsharp/day5/day5.fsx
+++ fsharp/day5/day5.fsx
@@ -6,6 +6,26 @@ type Instruction = {
   DestinationStack: int
 }
 
+let charToInt (c: char) =
+  int c - int '0'
+
+let parseInstruction (nums: int list) =
+  { NumberToMove = nums[0]; SourceStack = nums[1] - 1; DestinationStack = nums[2] - 1 }
+
+let splitInstruction (str: string) =
+  let fIndex = str |> Seq.toArray |> Array.findIndex (fun c -> c = 'f')
+  let (first, rest) = str |> Seq.toArray |> Array.splitAt fIndex
+  let tIndex = rest |> Seq.toArray |> Array.findIndex (fun c -> c = 't')
+  let (second, third) = rest |> Array.splitAt tIndex
+  [first; second; third]
+  |> List.map (fun arr -> Array.filter (fun c -> System.Char.IsNumber c) arr)
+  |> List.map (fun arr -> arr |> System.String |> int)
+
+let parseInstructions (inst: string list) =
+  inst
+  |> List.map splitInstruction
+  |> List.map parseInstruction
+
 let getCrates (cratesAndNums: string list) =
   cratesAndNums
   |> List.map Seq.toArray
@@ -22,6 +42,10 @@ let buildRows crates indices numstacks =
   crates
   |> List.map (fun row -> fullRow row indices numstacks)
 
+let trimStack stack =
+  stack
+  |> List.filter (fun c -> System.Char.IsLetter c)
+
 let rowsToStacks rows =
   let rec rowsToStacksRec (rows: char[] list) (stacks: char list list) =
     match rows with
@@ -35,11 +59,22 @@ let rowsToStacks rows =
         |> List.map Array.exactlyOne
         |> fun stack ->
           rowsToStacksRec (rows |> List.map (fun a -> Array.removeAt 0 a)) (stack::stacks)
-  rowsToStacksRec rows [] |> List.map List.rev |> List.rev
+  rowsToStacksRec rows [] |> List.rev |> List.map trimStack |> List.map List.toArray |> List.toArray
 
-let usefulIndices = [1;5;9]
+let applyInstruction (stacks: char[][]) instruction =
+  let crates = Array.take instruction.NumberToMove stacks[instruction.SourceStack] |> Array.rev
+  stacks[instruction.DestinationStack] <- Array.concat [crates; stacks[instruction.DestinationStack]]
+  stacks[instruction.SourceStack] <- Array.removeManyAt 0 instruction.NumberToMove stacks[instruction.SourceStack]
 
-let lines = File.ReadAllLines("fsharp/day5/test.txt") |> Array.toList
+let topCrate (stacks: char[][]) =
+  stacks
+  |> Array.map (Array.take 1)
+  |> Array.map Array.exactlyOne
+  |> System.String
+
+let usefulIndices = [1;5;9;13;17;21;25;29;33]
+
+let lines = File.ReadAllLines("fsharp/day5/input.txt") |> Array.toList
 
 let (crates, stackNumbers, instructions) =
   lines
@@ -52,7 +87,13 @@ let numberOfStacks =
   |> Seq.toArray
   |> Array.filter (fun c -> System.Char.IsNumber c)
   |> Array.last
-  |> fun c -> int c - int '0'
+  |> fun c -> charToInt c
 
 let rows = buildRows crates usefulIndices numberOfStacks
 let stacks = rowsToStacks rows
+let parsedInstructions = parseInstructions instructions
+
+parsedInstructions
+|> List.iter (fun i -> applyInstruction stacks i)
+
+let topCrates = topCrate stacks
