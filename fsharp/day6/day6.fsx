86d5e1a0ec5d67b6c0512698f63386acafa5ebc1
Patrick Johnstone <patrick.johnstone@spektrix.com>
2022-12-06 08:34:39 +0000

    Day6: part1 solved

---
+++ fsharp/day6/day6.fsx
@@ -0,0 +1,22 @@
+open System.IO
+
+let lines =
+  File.ReadAllLines "fsharp/day6/input.txt"
+  |> Array.toList
+  |> List.map Seq.toArray
+
+let isMarker letters =
+  if (Array.distinct letters).Length = letters.Length then true else false
+
+let findMarker (line: char[]) windowSize =
+  let rec findMarkerRec (line: char[]) index =
+    match line with
+    | [||] -> index + windowSize
+    | _ ->
+      match (isMarker (Array.take windowSize line)) with
+      | true -> index + windowSize
+      | false ->
+        findMarkerRec (Array.removeAt 0 line) (index + 1)
+  findMarkerRec line 0
+
+let firstMarkers = lines |> List.map (fun line -> findMarker line 4)
