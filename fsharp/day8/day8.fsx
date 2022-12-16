8f82a0d4228bda7dbd28d437d4e605a9d0ca1c8c
Patrick Johnstone <patrick.johnstone@spektrix.com>
2022-12-16 15:39:21 +0000

    day8 part2 solved real input

--- fsharp/day8/day8.fsx
+++ fsharp/day8/day8.fsx
@@ -3,10 +3,10 @@ open System.IO
 let flatten (A:'a[,]) = A |> Seq.cast<'a>
 
 let getColumn c (A:_[,]) =
-    flatten A.[*,c..c] |> Seq.toArray
+  flatten A.[*,c..c] |> Seq.toArray
 
 let getRow r (A:_[,]) =
-    flatten A.[r..r,*] |> Seq.toArray
+  flatten A.[r..r,*] |> Seq.toArray
 
 let isOnEdge x y grid =
   if x = 0 || y = 0 || x = ((getRow x grid).Length - 1) || y = ((getColumn y grid).Length - 1)
@@ -30,6 +30,28 @@ let visible x y (grid: int[,]) =
   then true
   else false
 
+let scenicScore num (array: int[]) =
+  if array.Length = 0 then 0
+  else
+    let treesUntilBlocked = array |> Array.takeWhile (fun e -> num > e)
+    if treesUntilBlocked.Length = array.Length then treesUntilBlocked.Length
+    else treesUntilBlocked.Length + 1
+
+let scenicLeft x y (grid: int[,]) =
+  (getRow x grid) |> Array.take y |> Array.rev |> scenicScore grid[x,y]
+
+let scenicRight x y (grid: int[,]) =
+  (getRow x grid) |> Array.removeManyAt 0 (y + 1) |> scenicScore grid[x,y]
+
+let scenicTop x y (grid: int[,]) =
+  (getColumn y grid) |> Array.take x |> Array.rev |> scenicScore grid[x,y]
+
+let scenicBottom x y (grid: int[,]) =
+  (getColumn y grid) |> Array.removeManyAt 0 (x + 1) |> scenicScore grid[x,y]
+
+let totalScenic x y grid =
+  (scenicLeft x y grid) * (scenicRight x y grid) * (scenicTop x y grid) * (scenicBottom x y grid)
+
 let lines = File.ReadAllLines "fsharp/day8/input.txt"
 
 let numbers =
@@ -40,10 +62,10 @@ let numbers =
 
 let grid = Array2D.init lines.Length lines[0].Length (fun x y -> numbers[x][y])
 
-let hopefullyTrue = visible 2 1 grid
-let hopefullyFalse = visible 1 3 grid
-
-
 let mutable numVisible = 0
 
-grid |> Array2D.iteri (fun x y v -> if visible x y grid then numVisible <- (numVisible + 1))
+grid |> Array2D.iteri (fun x y _ -> if visible x y grid then numVisible <- (numVisible + 1))
+
+let mutable highestScenicScore = 0
+
+grid |> Array2D.iteri (fun x y _ -> if (totalScenic x y grid) > highestScenicScore then highestScenicScore <- (totalScenic x y grid))
