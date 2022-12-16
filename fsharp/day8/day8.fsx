5754153d03cc26aadfb37e7152ba6a0f2f76aa7a
Patrick Johnstone <patrick.johnstone@spektrix.com>
2022-12-16 12:02:37 +0000

    day8 part1 row and column stuff

--- fsharp/day8/day8.fsx
+++ fsharp/day8/day8.fsx
@@ -1,5 +1,19 @@
 open System.IO
 
+let flatten (A:'a[,]) = A |> Seq.cast<'a>
+
+let getColumn c (A:_[,]) =
+    flatten A.[*,c..c] |> Seq.toArray
+
+let getRow r (A:_[,]) =
+    flatten A.[r..r,*] |> Seq.toArray
+
+let visibleFromLeft x y (grid: int[,]) =
+  if ((getRow x grid) |> Array.take y |> Array.max) < grid[x,y] then true else false
+
+let visibleFromTop x y (grid: int[,]) =
+  if ((getColumn y grid) |> Array.take x |> Array.max) < grid[x,y] then true else false
+
 let lines = File.ReadAllLines "fsharp/day8/test.txt"
 
 let numbers =
@@ -9,3 +23,8 @@ let numbers =
     a |> Array.map (fun c -> (c |> int) - 48))
 
 let grid = Array2D.init lines.Length lines[0].Length (fun x y -> numbers[x][y])
+
+let hopefullyTrue = visibleFromLeft 0 3 grid
+let hopefullyFalse = visibleFromLeft 0 2 grid
+let hopefullyTrue2 = visibleFromTop 2 0 grid
+let hopefullyFalse2 = visibleFromTop 1 0 grid
