9617484fce619abcf53948322603c97e749e6878
Patrick Johnstone <patrick.johnstone@spektrix.com>
2023-02-10 10:52:41 +0000

    Day9 part1: solved example

--- fsharp/day9/day9.fsx
+++ fsharp/day9/day9.fsx
@@ -1,8 +1,12 @@
 open System.IO
 
+let mutable history = []
+
 let initGrid size =
   let grid = Array2D.create size size [||]
   grid[(size - 1),0] <- [|'H'; 'T'; 's'|]
+  history <- ('H',(size - 1,0))::history
+  history <- ('T',(size - 1,0))::history
   grid
 
 let isPieceAtPos y x piece (grid: char[][,]) =
@@ -22,7 +26,9 @@ let takePieceFromPos y x piece (grid: char[][,]) =
   |> List.toArray
 
 let putPieceAtPos y x piece (grid: char[][,]) =
+  printfn "Putting %c at %i,%i" piece y x
   grid[y,x] <- Array.insertAt 0 piece grid[y,x]
+  history <- (piece,(y,x))::history
 
 let movePiece piece direction (grid: char[][,]) =
   let (currentY, currentX) = getIndexOfPiece piece grid
@@ -32,17 +38,28 @@ let movePiece piece direction (grid: char[][,]) =
   | 'U' -> putPieceAtPos (currentY - 1) currentX piece grid
   | 'L' -> putPieceAtPos currentY (currentX - 1) piece grid
   | _ -> putPieceAtPos currentY (currentX + 1) piece grid
-  printfn "%A" grid
-
-let runInstruction grid (dir, num) =
-  for _ = 1 to num do
-    movePiece 'H' dir grid
 
 let isTailAdjacent grid =
   let (tailY, tailX) = getIndexOfPiece 'T' grid
   let (headY, headX) = getIndexOfPiece 'H' grid
   if headY - tailY > 1 || tailY - headY > 1 || headX - tailX > 1 || tailX - headX > 1 then false
   else true
+
+let makeTailAdjacent dir grid =
+  let (currentY, currentX) = getIndexOfPiece 'H' grid
+  let (tailY, tailX) = getIndexOfPiece 'T' grid
+  takePieceFromPos tailY tailX 'T' grid
+  match dir with
+  | 'D' -> putPieceAtPos (currentY - 1) currentX 'T' grid
+  | 'U' -> putPieceAtPos (currentY + 1) currentX 'T' grid
+  | 'L' -> putPieceAtPos currentY (currentX + 1) 'T' grid
+  | _ -> putPieceAtPos currentY (currentX - 1) 'T' grid
+
+let runInstruction grid (dir, num) =
+  for _ = 1 to num do
+    movePiece 'H' dir grid
+    if not (isTailAdjacent grid) then
+      makeTailAdjacent dir grid
 
 let lines = File.ReadAllLines "fsharp/day9/test.txt" |> Array.toList
 
@@ -55,8 +72,10 @@ let instructions =
     |> Array.exactlyOne)
   |> List.map (fun (d,n) -> ((d |> Seq.toArray |> Array.exactlyOne), int n))
 
-let grid = initGrid 6
+let size = (instructions |> List.map (fun (_,i) -> i) |> List.max) + 1
+
+let grid = initGrid size
 
 instructions |> List.iter (fun i -> runInstruction grid i)
 
-isTailAdjacent grid
+history |> List.filter (fun (p,_) -> p = 'T') |> List.distinct |> List.length
