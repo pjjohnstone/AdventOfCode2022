2ebf3137dd49440430e714fb949c9c989488047f
Patrick Johnstone <patrick.johnstone@spektrix.com>
2023-02-10 15:05:47 +0000

    Day9 part2: refactor adjacency to be more generic

--- fsharp/day9/day9.fsx
+++ fsharp/day9/day9.fsx
@@ -27,6 +27,7 @@ let takePieceFromPos y x piece (grid: char[][,]) =
   |> List.toArray
 
 let putPieceAtPos y x piece (grid: char[][,]) =
+  printfn "Moving %c to %i,%i" piece y x
   grid[y,x] <- Array.insertAt 0 piece grid[y,x]
   history <- (piece,(y,x))::history
 
@@ -39,21 +40,27 @@ let movePiece piece direction (grid: char[][,]) =
   | 'L' -> putPieceAtPos currentY (currentX - 1) piece grid
   | _ -> putPieceAtPos currentY (currentX + 1) piece grid
 
-let isTailAdjacent grid =
-  let (tailY, tailX) = getIndexOfPiece 'T' grid
-  let (headY, headX) = getIndexOfPiece 'H' grid
-  if headY - tailY > 1 || tailY - headY > 1 || headX - tailX > 1 || tailX - headX > 1 then false
+let isAdjacent headPiece tailPiece grid =
+  let (tailPieceY, tailPieceX) = getIndexOfPiece tailPiece grid
+  let (headY, headX) = getIndexOfPiece headPiece grid
+  if headY - tailPieceY > 1 || tailPieceY - headY > 1 || headX - tailPieceX > 1 || tailPieceX - headX > 1 then false
   else true
 
-let makeTailAdjacent dir grid =
-  let (currentY, currentX) = getIndexOfPiece 'H' grid
-  let (tailY, tailX) = getIndexOfPiece 'T' grid
-  takePieceFromPos tailY tailX 'T' grid
+let isTailAdjacent grid =
+  isAdjacent 'H' 'T' grid
+
+let makeAdjacent dir piece grid =
+  let (headY, headX) = getIndexOfPiece 'H' grid
+  let (pieceY, pieceX) = getIndexOfPiece piece grid
+  takePieceFromPos pieceY pieceX 'T' grid
   match dir with
-  | 'D' -> putPieceAtPos (currentY - 1) currentX 'T' grid
-  | 'U' -> putPieceAtPos (currentY + 1) currentX 'T' grid
-  | 'L' -> putPieceAtPos currentY (currentX + 1) 'T' grid
-  | _ -> putPieceAtPos currentY (currentX - 1) 'T' grid
+  | 'D' -> putPieceAtPos (headY - 1) headX 'T' grid
+  | 'U' -> putPieceAtPos (headY + 1) headX 'T' grid
+  | 'L' -> putPieceAtPos headY (headX + 1) 'T' grid
+  | _ -> putPieceAtPos headY (headX - 1) 'T' grid
+
+let makeTailAdjacent dir grid =
+  makeAdjacent dir 'T' grid
 
 let runInstruction grid (dir, num) =
   for _ = 1 to num do
@@ -71,7 +78,7 @@ let maxPossibleSize instructions =
   let sizes = [(maxInDir instructions 'U'); (maxInDir instructions 'L'); (maxInDir instructions 'R'); (maxInDir instructions 'D')]
   sizes |> List.max
 
-let lines = File.ReadAllLines "fsharp/day9/input.txt" |> Array.toList
+let lines = File.ReadAllLines "fsharp/day9/test.txt" |> Array.toList
 
 let instructions =
   lines
@@ -81,6 +88,8 @@ let instructions =
     |> Array.pairwise
     |> Array.exactlyOne)
   |> List.map (fun (d,n) -> ((d |> Seq.toArray |> Array.exactlyOne), int n))
+
+printfn "Grid size will be: %i" (maxPossibleSize instructions)
 
 let grid = initGrid (maxPossibleSize instructions)
 
