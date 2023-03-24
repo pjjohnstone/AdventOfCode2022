8d9cdc3a9668fadd542174f5b1286061e7da5c95
Patrick Johnstone <patrick.johnstone@spektrix.com>
2023-03-24 12:46:21 +0000

    Day10 part1: big refactor, still not running commands to completion

--- fsharp/day10/day10.fsx
+++ fsharp/day10/day10.fsx
@@ -8,7 +8,7 @@ type Instruction = {
 
 type RegisterState = {
   X: int
-  Instructions: Instruction list
+  Instruction: Instruction
 }
 
 let parseInstruction (ins: string) =
@@ -21,47 +21,44 @@ let parseInstruction (ins: string) =
     |> fun (_,v) ->
       { Label = "addx"; Value = (v |> System.String |> int); CyclesRemain = 2}
 
+let tick instruction =
+  { instruction with CyclesRemain = instruction.CyclesRemain - 1 }
+
 let runInstruction instruction =
   match instruction.CyclesRemain with
   | 0 -> (Some(instruction.Value), None)
   | _ -> (None, Some(instruction))
 
-let decrementInstructions instructions =
-  instructions
-  |> List.map (fun i -> {i with CyclesRemain = i.CyclesRemain - 1})
-
-let executeCycle register =
-  let processedInstructions =
-    register.Instructions
-    |> List.map (fun i -> runInstruction i)
+let executeCycle register (buffer: Instruction list) =
+  let (newXOption, newInstOption) = runInstruction register.Instruction
   let newXValue =
-    processedInstructions
-    |> List.map (fun (x,_) -> x)
-    |> List.choose id
-    |> List.sum
-  let instructions =
-    processedInstructions
-    |> List.map (fun (_,i) -> i)
-    |> List.choose id
-    |> decrementInstructions
-  { X = register.X + newXValue; Instructions = instructions }
+    match newXOption with
+    | None -> 0
+    | Some(x) -> x
+  match newInstOption with
+  | None ->
+    ({ X = register.X + newXValue; Instruction = buffer.Head}, buffer.Tail)
+  | Some(i) ->
+    ({ X = register.X + newXValue; Instruction = i }, buffer)
 
-let processRegister buffer =
+let processRegister (buffer: Instruction list) =
   let rec processRegisterRec (buffer: Instruction list) register history cycle =
+    let tickedRegister = { register with Instruction = (tick register.Instruction )}
     match buffer with
     | [] ->
-      match register.Instructions with
-      | [] -> ((cycle, register)::history)
+      match tickedRegister.Instruction.CyclesRemain with
+      | 0 -> ((cycle, tickedRegister)::history)
       | _ ->
-        processRegisterRec [] (executeCycle register) ((cycle, register)::history) (cycle + 1)
+        let (newRegState, newBuffer) = executeCycle tickedRegister buffer
+        processRegisterRec newBuffer newRegState ((cycle, tickedRegister)::history) (cycle + 1)
     | _ ->
-      processRegisterRec buffer.Tail (executeCycle { register with Instructions = register.Instructions@[buffer.Head] }) ((cycle, register)::history) (cycle + 1)
-  processRegisterRec buffer { X = 1; Instructions = []} [] 0
+      let (newRegState, newBuffer) = executeCycle tickedRegister buffer
+      processRegisterRec newBuffer newRegState ((cycle, tickedRegister)::history) (cycle + 1)
+  processRegisterRec buffer.Tail { X = 1; Instruction = buffer.Head} [] 1
 
 let prettyPrintRegisterState state =
   let mutable string = ""
-  for instruction in state.Instructions do
-    string <- string + $" {instruction.Label} {instruction.Value} {instruction.CyclesRemain}"
+  string <- string + $" {state.Instruction.Label} {state.Instruction.Value} {state.Instruction.CyclesRemain}"
   printfn "X: %i Instructions: %s" state.X string
 
 let getStateAtCycle (history: (int * RegisterState) list) cycle =
@@ -78,22 +75,24 @@ let getValueAtCycle (history: (int * RegisterState) list) cycle =
   |> List.exactlyOne
   |> fun (_,r) -> r.X
 
-let lines = File.ReadAllLines("fsharp/day10/test.txt") |> Array.toList
+let lines = File.ReadAllLines("fsharp/day10/simpletest.txt") |> Array.toList
 let buffer = lines |> List.map parseInstruction
 let history = processRegister buffer
 
 printfn "%A" history
-getStateAtCycle history 0
+// getStateAtCycle history 0
 getStateAtCycle history 1
 getStateAtCycle history 2
 getStateAtCycle history 3
 getStateAtCycle history 4
 getStateAtCycle history 5
-printfn "Value of X at cycle %i was %i" 18 (getValueAtCycle history 18)
-printfn "Value of X at cycle %i was %i" 19 (getValueAtCycle history 19)
-printfn "Value of X at cycle %i was %i" 20 (getValueAtCycle history 20)
-printfn "Value of X at cycle %i was %i" 21 (getValueAtCycle history 21)
-printfn "Value of X at cycle %i was %i" 22 (getValueAtCycle history 22)
+// getStateAtCycle history 6
+// getStateAtCycle history 7
+// printfn "Value of X at cycle %i was %i" 18 (getValueAtCycle history 18)
+// printfn "Value of X at cycle %i was %i" 19 (getValueAtCycle history 19)
+// printfn "Value of X at cycle %i was %i" 20 (getValueAtCycle history 20)
+// printfn "Value of X at cycle %i was %i" 21 (getValueAtCycle history 21)
+// printfn "Value of X at cycle %i was %i" 22 (getValueAtCycle history 22)
 // printfn "Value of X at cycle %i was %i" 60 (getValueAtCycle history 60)
 // printfn "Value of X at cycle %i was %i" 100 (getValueAtCycle history 100)
 // printfn "Value of X at cycle %i was %i" 140 (getValueAtCycle history 140)
