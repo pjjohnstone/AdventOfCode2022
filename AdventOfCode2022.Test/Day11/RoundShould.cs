bdae63dcedc3ddfe4b1fcc6dce632328c5ef57ee
Patrick Johnstone <johnstonep@gmail.com>
2023-04-18 20:13:20 +0100

    Day11 Part1: init rounds

--- AdventOfCode2022.Test/Day11/RoundShould.cs
+++ AdventOfCode2022.Test/Day11/RoundShould.cs
@@ -1,16 +1,23 @@
 ﻿using AdventOfCode2022.Core.Day11;
 using AdventOfCode2022.Core.Day11.InspectionStrategies;
+using Moq;
 
 namespace AdventOfCode2022.Test.Day11;
 
 [TestFixture]
 public class RoundShould
 {
-  [Test,Ignore("WIP")]
+  [Test]
   public void Call_MonkeyParser_To_Initialise_Monkeys()
   {
     var monkey0 = new Monkey(0, new List<int>(), new MultiplyStrategy(3));
-    var round = new Round();
-    Assert.That(round.Monkeys.Contains(monkey0));
+    var parser = new Mock<IMonkeyParser>();
+    var inputData = new List<string> { "test" };
+    parser.Setup(p => p.Monkeys(It.Is<IEnumerable<string>>(d => d.Equals(inputData)))).Returns(new List<Monkey>{monkey0});
+    var round = new Round(parser.Object, inputData);
+
+    round.Initialise();
+
+    Assert.That(round.Monkeys, Does.Contain(monkey0));
   }
 }
--- AdventofCode2022.Core/Day11/Round.cs
+++ AdventofCode2022.Core/Day11/Round.cs
@@ -2,5 +2,20 @@
 
 public class Round
 {
-  public List<Monkey> Monkeys { get; set; }
+  private readonly IMonkeyParser _parser;
+  private readonly IEnumerable<string> _inputData;
+
+  public Round(IMonkeyParser parser, IEnumerable<string> inputData)
+  {
+    _parser = parser;
+    _inputData = inputData;
+    Monkeys = new List<Monkey>();
+  }
+
+  public List<Monkey> Monkeys { get; private set; }
+
+  public void Initialise()
+  {
+    Monkeys = _parser.Monkeys(_inputData).ToList();
+  }
 }
