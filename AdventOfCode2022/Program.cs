06230f42f2d5781868b31fe4737fced2d03acc60
Patrick Johnstone <patrick.johnstone@spektrix.com>
2023-04-20 08:46:27 +0100

    Day11 Part1 solved

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using AdventOfCode2022.Core.Day11;
+
+Console.WriteLine("Day11, Part1");
+var inputData = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Day11/input.txt")).Where(s => !string.IsNullOrEmpty(s));
+var round = new Round(new MonkeyParser(), inputData);
+round.Initialise();
+for (int i = 0; i < 20; i++)
+{
+  round.PlayRound();
+}
+var topMonkeys = round.Monkeys.OrderByDescending(m => m.Inspections).Take(2);
+Console.WriteLine($"Monkey business this round is: {topMonkeys.Select(m => m.Inspections).Aggregate((x,y) => x * y)}");
+
