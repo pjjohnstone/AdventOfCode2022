efa601504732cd3fa99ef217f4b761a155b4c068
Patrick Johnstone <patrick.johnstone@spektrix.com>
2023-05-05 13:32:24 +0100

    strip matching characters

--- AdventOfCode2022.FSharp/Day13/Packets.fs
+++ AdventOfCode2022.FSharp/Day13/Packets.fs
@@ -1,41 +1,50 @@
 ﻿module Packets
 
 type Result = {
-  Pair: char * char
+  Pair: char list * char list
   InOrder: bool option
 }
 
-let numberComparison (pair: char * char) =
-  let (left,right) = pair
-  match System.Int32.TryParse (left |> System.String.Concat) with
-  | true,intLeft ->
-    match System.Int32.TryParse (right |> System.String.Concat) with
-    | true,intRight ->
-      match intLeft <= intRight with
-      | true ->
-        { Pair = (left,right); InOrder = Some(true) }
-      | false ->
-        { Pair = (left,right); InOrder = Some(false) }
+let areSame (pair: char list * char list) =
+  let rec areSameRec (pair: char list * char list) =
+    let (left,right) = pair
+    match left.Head = right.Head with
+    | false ->
+      { Pair = pair; InOrder = None }
+    | true ->
+      areSameRec (left.Tail, right.Tail)    
+  areSameRec pair
+
+let numberComparison result =
+  match result.InOrder with
+  | Some(_) -> result
+  | None ->
+    let (left,right) = result.Pair
+    match System.Int32.TryParse (left.Head |> System.String.Concat) with
+    | true,intLeft ->
+      match System.Int32.TryParse (right.Head |> System.String.Concat) with
+      | true,intRight ->
+        match intLeft <= intRight with
+        | true ->
+          { Pair = (left,right); InOrder = Some(true) } // both numbers left smaller
+        | false ->
+          { Pair = (left,right); InOrder = Some(false) } // both numbers left bigger
+      | false,_ ->
+        { Pair = (left,right); InOrder = None } // right is not a number
     | false,_ ->
-      { Pair = (left,right); InOrder = None }
-  | false,_ ->
-    { Pair = (left,right); InOrder = None }
+      { Pair = (left,right); InOrder = None } // neither are numbers
 
 let evaluateRules pair =
   pair
+  |> areSame
   |> numberComparison
 
 let inOrder (pair: string * string) =
-  let rec inOrderRec (pair: char list * char list) =
-    let (left,right) = pair
-    match (left.Head = right.Head) with
-    | true -> 
-      inOrderRec (left.Tail, right.Tail)
-    | false ->
-      match (evaluateRules (left.Head, right.Head)).InOrder with
-      | Some(true) -> true
-      | Some(false) -> false
-      | _ ->
-        inOrderRec (left.Tail, right.Tail)
   let (left,right) = pair
-  inOrderRec (left |> Seq.toArray |> Array.toList, right |> Seq.toArray |> Array.toList)
+  let charPairs = (left |> Seq.toArray |> Array.toList, right |> Seq.toArray |> Array.toList)
+  let result = evaluateRules charPairs
+  match result.InOrder with
+  | Some(true) -> true
+  | Some(false) -> false
+  | _ -> true
+  
